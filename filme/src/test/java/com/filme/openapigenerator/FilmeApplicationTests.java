c1b3a6ee79452e6c1d05c0fc911869c1f0455719
Paulo Nascimento <paulo.nascimento@verity.com.br>
2023-01-23 11:16:00 -0300

    escopo do projeto teste 1

---
+++ filme/src/test/java/com/filme/openapigenerator/FilmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filme.openapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
